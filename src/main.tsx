4db0c9dd167ea40b24f18c7fd552fc441a1e2183
Giedrius <49316774+Longas13@users.noreply.github.com>
2022-10-21 00:18:08 +0300

    feat: add i18n. add translation strings to list

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ReactQueryClientProvider } from "./lib/react-query";
+import I18nService from "./services/I18nService";
+
+I18nService.init();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
