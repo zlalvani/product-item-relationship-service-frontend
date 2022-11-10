796b82c236ad34168a54d6447584249030c3be2f
Neal Burger <neal.buerger@accenture.com>
2022-11-10 14:29:52 +0100

    feat: helper for rendering with router

--- src/testing/test-utils.tsx
+++ src/testing/test-utils.tsx
@@ -6,7 +6,9 @@ import { Provider } from "react-redux";
 import { serverEnvReducer } from "../store/serverEnvironment";
 import { AppStore, RootState } from "../store/store";
 
+import userEvent from "@testing-library/user-event";
 import { I18nextProvider } from "react-i18next";
+import { BrowserRouter } from "react-router-dom";
 import { ReactQueryTestClientProvider } from "../lib";
 import i18n from "./i18n-testconfig";
 
@@ -44,6 +46,15 @@ export function renderCustomHook<Props, Result>(fn: (props: Props) => Result) {
   );
   return renderHook(fn, { wrapper });
 }
+
+export const renderWithRouter = (ui: React.ReactElement, { route = "/" } = {}) => {
+  window.history.pushState({}, "Test page", route);
+
+  return {
+    user: userEvent.setup(),
+    ...renderWithProviders(ui, { wrapper: BrowserRouter }),
+  };
+};
 
 export * from "@testing-library/react";
 export { renderWithProviders as render };
