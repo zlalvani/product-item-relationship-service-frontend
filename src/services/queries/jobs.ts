ec26300cef09cf00dc39119df6f588731ddc456f
Neal Burger <neal.buerger@accenture.com>
2022-11-14 13:40:08 +0100

    feat: add support for refetch on environment change

--- src/services/queries/jobs.ts
+++ src/services/queries/jobs.ts
@@ -1,15 +1,18 @@
 import { useMutation } from "@tanstack/react-query";
 import { useQuery } from "../../lib/react-query";
+import { useAppSelector } from "../../store/store";
 import { cancelJob, fetchJobById, fetchJobs } from "../api/jobs.api";
 
 export const useFetchJobById = (id: string, refetchInterval: false | number = false) => {
-  return useQuery(["jobs", id], () => fetchJobById(id), {
+  const { serverEnv } = useAppSelector((state) => state.serverEnvReducer);
+  return useQuery(["jobs", serverEnv, id], () => fetchJobById(id), {
     refetchInterval,
   });
 };
 
 export const useFetchJobs = (refetchInterval: false | number = false) => {
-  return useQuery(["jobs"], () => fetchJobs(), { refetchInterval });
+  const { serverEnv } = useAppSelector((state) => state.serverEnvReducer);
+  return useQuery(["jobs", serverEnv], () => fetchJobs(), { refetchInterval });
 };
 
 export const useCancelJobs = () => {
