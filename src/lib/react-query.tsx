1ddaa9e8e9ed0060f09a34ad932502eb247812e0
Neal Burger <neal.buerger@accenture.com>
2023-01-23 16:39:30 +0100

    feat: cleaner cancel job

--- src/lib/react-query.tsx
+++ src/lib/react-query.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
 export const ReactQueryClientProvider: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
