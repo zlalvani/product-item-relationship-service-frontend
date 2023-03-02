09a0f84f6261bfc87e4e369df30449f292101376
Emil Cristurean <Emil.Cristurean@partner.bmw.de>
2023-03-02 09:55:36 +0200

    feat: refactor docker image

---
+++ vite.config.prod.ts
@@ -0,0 +1,29 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import inject from '@rollup/plugin-inject';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [checker({ typescript: true }), react()],
+  server: {
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 8080,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest-setup.js",
+    coverage: {
+      include: ["src/**"],
+      exclude: ["src/generated/**"],
+      provider: 'istanbul', // or 'c8'
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+  build: {
+    rollupOptions: {
+        plugins: [inject({ Buffer: ['Buffer', 'Buffer'] })],
+    },
+},
+});
