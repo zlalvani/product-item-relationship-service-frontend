6352e7babefe3a29454d26983204046eb2bfcaac
Zaran Lalvani <zaran@xeol.io>
2024-04-04 16:53:53 -0400

    chore: prettier

--- src/services/queries/user.ts
+++ src/services/queries/user.ts
@@ -2,5 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { keycloak } from "../../lib/keycloak";
 
 export const getUserInfo = () => {
-  return useQuery({ queryKey: ["userInfo"], queryFn: async () => { return (await keycloak.loadUserInfo()) as { name: string; tenant: string }; } });
+  return useQuery({
+    queryKey: ["userInfo"],
+    queryFn: async () => {
+      return (await keycloak.loadUserInfo()) as { name: string; tenant: string };
+    },
+  });
 };
